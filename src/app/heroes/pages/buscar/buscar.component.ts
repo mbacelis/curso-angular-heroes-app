3fb7df3b876a30b1d069b3f4bc870f247112c3c3
Mauricio Bacelis <mbacelis_@hotmail.com>
2022-07-18 17:32:51 -0500

    Se implementa busqueda y selección de elementos en el autocomplete

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -13,20 +13,26 @@ export class BuscarComponent implements OnInit {
 
   termino: string = '';
   heroes: Heroe[] = [];
-  heroeSeleccionado!: Heroe;
+  heroeSeleccionado: Heroe | undefined;
 
   constructor(private heroeService: HerosService) { }
 
   ngOnInit(): void {
-
   }
 
   buscando() {
-    this.heroeService.getSugerencias(this.termino)
+    this.heroeService.getSugerencias(this.termino.trim())
       .subscribe(heroes => this.heroes = heroes);
   }
 
   opcionSeleccionada(event: MatAutocompleteSelectedEvent) {
+
+    if (!event.option.value)
+    {
+      this.heroeSeleccionado = undefined;
+      return;
+    }
+
     const heroe: Heroe = event.option.value;
 
     this.termino = heroe.superhero;
